2067122eeb43106cf7dd422a467e346b7f36a350
Kalin Kiesling <krkiesling@gmail.com>
2018-03-07 16:30:45 -0600

    started first check point

---
+++ checkpoint0.py
@@ -0,0 +1,17 @@
+# need to import the "math" module to use different math functions
+import math as m
+
+# Lead shield 5 cm thick
+phi_0 = 1.e10 # #/cm**2-s
+mu = 0.125 # attenuation coefficient
+x = 5.0 # shield thickness in cm
+phi = phi_0 * m.exp(-mu*x)
+
+print(phi)
+
+
+# plastic shield 15 cm thick
+phi_0 = 1.e15
+mu = 0.02
+x = 15.
+phi = phi_0 * m.exp(-mu*x)
