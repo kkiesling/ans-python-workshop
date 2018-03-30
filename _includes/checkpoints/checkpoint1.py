cd81b6e99e0b1323201bdc430bf797b33fd27457
Kalin Kiesling <krkiesling@gmail.com>
2018-03-30 15:26:43 -0500

    attentuation coeff unit

--- _includes/checkpoints/checkpoint1.py
+++ _includes/checkpoints/checkpoint1.py
@@ -11,7 +11,7 @@ import math as m
 # Compute result after lead shield 5 cm thick with an initial flux of
 # 1e10 photons/cm**2-s
 phi_0 = 1.e10   # photons/cm**2-s
-mu = 0.125      # attenuation coefficient
+mu = 0.125      # attenuation coefficient 1/cm
 x = 5.0         # shield thickness in cm
 phi_lead = phi_0 * m.exp(-mu*x)
 
@@ -21,7 +21,7 @@ print(phi_lead)
 # Compute result after plastic shield 15 cm thick with an initial flux
 # of 1e15 photons/cm**2-s
 phi_0 = 1.e15   # photons/cm**2-s
-mu = 0.02       # attenuation coefficient
+mu = 0.02       # attenuation coefficient 1/cm
 x = 15.0        # shield thickness in cm
 phi_plastic = phi_0 * m.exp(-mu*x)
 
