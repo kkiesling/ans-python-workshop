94588086f8da344c6ee2c0fcda826385c066ed91
Kalin Kiesling <krkiesling@gmail.com>
2018-03-30 14:31:38 -0500

    first three checkpoints barebones

--- checkpoint1.py
+++ checkpoint1.py
@@ -5,7 +5,15 @@ import math as m
 phi_0 = 1.e10 # #/cm**2-s
 mu = 0.125 # attenuation coefficient
 x = 5.0 # shield thickness in cm
-phi = phi_0 * m.exp(-mu*x)
+phi_lead = phi_0 * m.exp(-mu*x)
 
-print(phi)
+print(phi_lead)
 
+
+# plastic shield 15 cm thick
+phi = 1.e15
+mu = 0.02
+x = 15.
+phi_plastic = phi_0 * m.exp(-mu*x)
+
+print(phi_plastic)
