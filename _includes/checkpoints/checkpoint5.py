8929cd059c5f5094f7b0e4cd4a3069b27245ae24
Kalin Kiesling <krkiesling@gmail.com>
2018-03-30 15:51:50 -0500

    comments and added more scenarios for arbitrary number of shields

--- _includes/checkpoints/checkpoint5.py
+++ _includes/checkpoints/checkpoint5.py
@@ -1,26 +1,68 @@
-# need to import the "math" module to use different math functions
+"""
+Checkpoint 5: Shield made of arbitrary number of materials
+----------------------------------------------------------
+
+Pass a set of material properties representing any number of materials
+in a shield each with their own thickness and attenuation coefficient.
+"""
+
 import math as m
 
-def calc_phi(phi_0, mat_props):
+def calc_phi(phi, mat_props):
+    """Calculates final flux after two shields of different thicknesses
+    and attenuation coefficients given the starting flux phi_0.
 
+    Input:
+    ------
+        phi: float, initial flux value (photons/cm**2-s)
+        mat_props: list of tuples, each tuple represents a set of
+            properties for each material in the problem in the form
+            [(mu0, x0), (mu1, x1), (mu2, x2) ... (muN, xN)] for any N
+            number of shields. Each shielding material (one tuple)
+            should be defined by:
+                mu: float, attenuation coefficient (1/cm)
+                x: float, shield thickness (cm)
+
+    Returns:
+    --------
+        phi: float, final flux value after all shields (photons/cm**2-s)
+    """
+
+    # iterate over each shield in the property list
     for mat in mat_props:
+
+        # unpack current shield properties mu and x
         mu = mat[0]
         x = mat[1]
 
-        phi_0 = phi_0 * m.exp(-mu*x)
+        # update phi_0 with new flux value after attenuation through
+        # current shield
+        phi = phi * m.exp(-mu*x)
 
-    return(phi_0)
+    # return final flux value
+    return(phi)
 
 
-# Lead shield 5 cm thick followed by plastic shield 15 cm thick
-# followed by tungsten
-phi_0 = 1.e15
+# Set properties for three different shielding types (lead, plastic, and
+# tungsten:
 lead_props = (.125, 5.0)
 plastic_props = (.02, 15.)
 tungsten_props = (.120, 7.)
+
+# Set initial flux
+phi_0 = 1.e15
+
+# Calculate using three shields (lead, plastic, and tungsten)
 mat_props = [lead_props, plastic_props, tungsten_props]
-
 phi_final = calc_phi(phi_0, mat_props)
-
 print(phi_final)
 
+# Calculate using two shields (lead and tungsten)
+mat_props = [lead_props, tungsten_props]
+phi_final = calc_phi(phi_0, mat_props)
+print(phi_final)
+
+# Calculate using one shield (lead)
+mat_props = [lead_props]
+phi_final = calc_phi(phi_0, mat_props)
+print(phi_final)
